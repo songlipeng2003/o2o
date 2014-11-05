6c6a76d54d218cf798506893ab56e88d56343f6a
Thinking Song <123wuming_123@163.com>
2014-11-06 00:53:21 +0800

    add city manage

---
+++ app/admin/city.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register City do
+  permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    actions
+  end
+
+  filter :name
+
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
+end
