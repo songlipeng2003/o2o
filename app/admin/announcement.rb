2f5b10a2e3d17bc37abd1f668d094826086b7916
Thinking Song <123wuming_123@163.com>
2014-11-12 18:26:06 +0800

    add announcement manage

---
+++ app/admin/announcement.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register Announcement do
+  permit_params :title, :content
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    actions
+  end
+
+  filter :title
+  filter :content
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :content
+    end
+    f.actions
+  end
+
+end
