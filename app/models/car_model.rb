7840a84b3ae023e7e7dd12a82a2892c82d06aa09
Thinking Song <123wuming_123@163.com>
2014-12-08 13:17:24 +0800

    add car_model's name validate unique

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,6 +1,7 @@
 class CarModel < ActiveRecord::Base
     validates :name, presence: true
     validates :car_brand_id, presence: true
+    validates :name, uniqueness: { scope: :car_brand_id }
 
     belongs_to :car_brand
 end
