4ef103b3bd53fc35990f6cddb1a0b6be55568af9
Thinking Song <123wuming_123@163.com>
2014-12-22 00:26:23 +0800

    添加汽车品牌接口

--- app/api/didi/api.rb
+++ app/api/didi/api.rb
@@ -24,6 +24,23 @@ module Didi
       end
     end
 
+    resource :car_brands do
+      desc "汽车品牌接口"
+      get do
+        present CarBrand::order(:first_letter).all
+      end
+
+      desc "汽车品牌详情"
+      params do
+        requires :id, type: Integer, desc: "ID"
+      end
+      route_param :id do
+        get do
+          CarBrand.find(params[:id])
+        end
+      end
+    end
+
     resource :docs do
       desc "文档接口"
       get do
