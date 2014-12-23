7a54b085be8d5bdba3f47ae93216513e3eddeeb9
Thinking Song <123wuming_123@163.com>
2014-12-23 23:19:27 +0800

    add register api

--- app/models/auth_code.rb
+++ app/models/auth_code.rb
@@ -16,6 +16,12 @@ class AuthCode < ActiveRecord::Base
     code
   end
 
+  def self.validate_code(phone, code)
+    auth_code = self.where("phone=? AND code=?", phone, code).first
+
+    return !auth_code.blank? && auth_code.expired_at > Time.now
+  end
+
   private
   def self.gen_code
     (0..5).map { (1..9).to_a[rand(8)] }.join
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+
+  validates :phone, presence: true, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.random_email
+    name = (0..10).map { ('a'..'z').to_a[rand(8)] }.join
+
+    name + '@24didi.com'
+  end
 end
