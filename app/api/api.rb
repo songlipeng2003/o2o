15f248bd5c6c21f4ea23fb82fc504a60e334e855
Thinking Song <123wuming_123@163.com>
2014-12-27 12:16:06 +0800

    授权移入v1

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,42 +1,5 @@
 require 'grape-swagger'
 
 class API < Grape::API
-  default_format :json
-  format :json
-
-  before do
-    header['Access-Control-Allow-Origin'] = '*'
-    header['Access-Control-Request-Method'] = '*'
-  end
-
-  helpers do
-    def logger
-      API.logger
-    end
-  end
-
-  before do
-    error!("401 Unauthorized", 401) unless authenticated
-  end
-
-  helpers do
-    def warden
-      env['warden']
-    end
-
-    def authenticated
-      return true if warden.authenticated?
-      params[:access_token] && @user = User.find_by_authentication_token(params[:access_token])
-    end
-
-    def current_user
-      warden.user || @user
-    end
-  end
-
-  mount V1::Root => '/v1'
-
-  add_swagger_documentation hide_documentation_path: true,
-    base_path: '/api',
-    api_version: 'v1'
-  end
+  mount V1::Root
+end
--- app/api/v1/root.rb
+++ app/api/v1/root.rb
@@ -1,6 +1,40 @@
 module V1
   class Root < Grape::API
+    default_format :json
+    format :json
     error_formatter :json, V1::ErrorFormatter
+
+    version 'v1', using: :path
+
+    before do
+      header['Access-Control-Allow-Origin'] = '*'
+      header['Access-Control-Request-Method'] = '*'
+    end
+
+    helpers do
+      def logger
+        API.logger
+      end
+    end
+
+    before do
+      error!("401 Unauthorized", 401) unless authenticated
+    end
+
+    helpers do
+      def warden
+        env['warden']
+      end
+
+      def authenticated
+        return true if warden.authenticated?
+        params[:access_token] && @user = User.find_by_authentication_token(params[:access_token])
+      end
+
+      def current_user
+        warden.user || @user
+      end
+    end
 
     mount V1::Announcements
     mount V1::Areas
@@ -12,5 +46,9 @@ module V1
     mount V1::Communities
     mount V1::Docs
     mount V1::Orders
+
+    add_swagger_documentation hide_documentation_path: true,
+      base_path: '/api',
+      api_version: 'v1'
   end
 end
