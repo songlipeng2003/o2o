0d52a94764a6352dc28639266aa1f8e1f3ec42c1
Thinking Song <123wuming_123@163.com>
2014-12-29 21:35:23 +0800

    update api_url

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,6 +1,6 @@
 GrapeSwaggerRails.options.app_name = '接口平台'
 GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
-GrapeSwaggerRails.options.app_url  = 'http://0.0.0.0:3000'
+GrapeSwaggerRails.options.app_url  = ''
 # GrapeSwaggerRails.options.api_auth     = 'basic' # Or 'bearer' for OAuth
 # GrapeSwaggerRails.options.api_key_name = 'Authorization'
 # GrapeSwaggerRails.options.api_key_type = 'header'
