f593d784d037e4e079766aea07d4215dc2f13709
Thinking Song <123wuming_123@163.com>
2014-12-29 22:15:51 +0800

    add update user info api

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -10,6 +10,16 @@ module V1
           User.find(params[:id])
         end
       end
+
+      desc '更新用户信息'
+      params do
+        requires :avatar, type: String, desc: '头像'
+        requires :nickname, type: String, desc: '昵称'
+        requires :sex, type: String, desc: '性别'
+      end
+      put do
+
+      end
     end
   end
 end
