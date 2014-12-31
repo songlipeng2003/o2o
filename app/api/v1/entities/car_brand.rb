f1093e2a1da62341a55872aa9681db911de9a34b
Thinking Song <123wuming_123@163.com>
2014-12-31 16:24:57 +0800

    汽车品牌接口返回添加first_letter

--- app/api/v1/entities/car_brand.rb
+++ app/api/v1/entities/car_brand.rb
@@ -2,11 +2,13 @@ module V1
   module Entities
     class CarBrandCarModel < Grape::Entity
       expose :id
+      expose :first_letter
       expose :name
     end
 
     class CarBrand < Grape::Entity
       expose :id
+      expose :first_letter
       expose :name
       expose :car_models, using: V1::Entities::CarBrandCarModel
     end
