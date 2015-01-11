49bd76a1a6b6e664ba2b84e9448ff45d038183a7
Thinking Song <123wuming_123@163.com>
2015-01-11 21:51:26 +0800

    fix page undefined problem

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -1,3 +1,10 @@
 Kaminari.configure do |config|
-  config.page_method_name = :per_page_kaminari
+  # config.default_per_page = 25
+  # config.max_per_page = nil
+  # config.window = 4
+  # config.outer_window = 0
+  # config.left = 0
+  # config.right = 0
+  # config.page_method_name = :page
+  # config.param_name = :page
 end
