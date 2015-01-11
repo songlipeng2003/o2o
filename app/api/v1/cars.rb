2256dd09031675eaa1b267ef5b2aab7b590ad8dc
Thinking Song <123wuming_123@163.com>
2015-01-11 19:14:58 +0800

    add X-Access-Token header to car api

--- app/api/v1/cars.rb
+++ app/api/v1/cars.rb
@@ -5,12 +5,26 @@ module V1
     end
 
     resource :cars do
-      desc "汽车"
+      desc "汽车", {
+        headers: {
+          "X-Access-Token" => {
+            description: "Token",
+            required: true
+          },
+        }
+      }
       get do
         present current_user.cars.all
       end
 
-      desc "汽车详情"
+      desc "汽车详情", {
+        headers: {
+          "X-Access-Token" => {
+            description: "Token",
+            required: true
+          },
+        }
+      }
       params do
         requires :id, type: Integer, desc: "ID"
       end
@@ -20,18 +34,32 @@ module V1
         end
       end
 
-      desc "添加汽车"
+      desc "添加汽车", {
+        headers: {
+          "X-Access-Token" => {
+            description: "Token",
+            required: true
+          },
+        }
+      }
       params do
         requires :car_model_id, type: Integer, desc: "车型id"
         requires :license_tag, type: String, desc: "牌照"
-        requires :buy_date, type: Date, desc: "购买日期, 格式： 2014-01-01"
+        requires :buy_date, type: String, desc: "购买日期, 格式： 2014-01-01"
         requires :color, type: String, desc: "颜色,直接使用中文名称"
       end
       post do
-        present current_user.cars.create(params)
+        present current_user.cars.create(permitted_params)
       end
 
-      desc "编辑汽车"
+      desc "编辑汽车", {
+        headers: {
+          "X-Access-Token" => {
+            description: "Token",
+            required: true
+          },
+        }
+      }
       params do
         requires :id, type: Integer, desc: "ID"
         requires :car_model_id, type: Integer, desc: "车型id"
@@ -42,11 +70,18 @@ module V1
       route_param :id do
         put do
           car = current_user.cars.find(params[:id])
-          present car.update(params)
+          present car.update(permitted_params)
         end
       end
 
-      desc "删除汽车"
+      desc "删除汽车", {
+        headers: {
+          "X-Access-Token" => {
+            description: "Token",
+            required: true
+          },
+        }
+      }
       params do
         requires :id, type: Integer, desc: "ID"
       end
