542178051e49c49eadb292717b4864985c7c3248
Thinking Song <123wuming_123@163.com>
2015-01-13 15:25:47 +0800

    添加服务范围监测接口

--- app/api/v1/stores.rb
+++ app/api/v1/stores.rb
@@ -2,13 +2,23 @@ module V1
   class Stores < Grape::API
 
     resource :stores do
-      desc "根据经纬度获取能够提供服务的店铺,未实现，暂时返回第一个店铺"
+      desc "根据经纬度获取是否提供服务器，未实现，暂时返回可以"
       params do
         requires :lon, type: Float, desc: "经度"
         requires :lat, type: Float, desc: "纬度"
       end
-      get 'search' do
-        present Store.page(1).per(1)
+      get 'in_service_scope' do
+        { code: 0 }
+      end
+
+      desc "根据经纬度和服务时间获取是否提供服务器，未实现，暂时返回可以"
+      params do
+        requires :lon, type: Float, desc: "经度"
+        requires :lat, type: Float, desc: "纬度"
+        requires :booked_at, type: String, desc: "服务时间"
+      end
+      get 'can_service' do
+        { code: 0 }
       end
     end
   end
