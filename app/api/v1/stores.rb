7d7df88cf812496df0e2b9a30a6b016e266eda20
Thinking Song <123wuming_123@163.com>
2015-01-19 09:47:46 +0800

    实现是否在服务区接口

--- app/api/v1/stores.rb
+++ app/api/v1/stores.rb
@@ -8,7 +8,7 @@ module V1
         requires :lat, type: Float, desc: "纬度"
       end
       get 'in_service_scope' do
-        { code: 0 }
+        Store.in_service_scope(params[:lon], params[:lat])
       end
 
       desc "根据经纬度和服务时间获取是否提供服务器，未实现，暂时返回可以"
@@ -18,7 +18,7 @@ module V1
         requires :booked_at, type: String, desc: "服务时间"
       end
       get 'can_service' do
-        { code: 0 }
+        # { code: 0 }
       end
     end
   end
--- app/models/store.rb
+++ app/models/store.rb
@@ -5,4 +5,63 @@ class Store < ActiveRecord::Base
   validates :description, presence: true
   validates :lon, presence: true, numericality: true
   validates :lat, presence: true, numericality: true
+
+  def self.in_service_scope(lon, lat)
+    query = {
+      query:{
+        filtered: {
+          query: {
+            match_all: {}
+          },
+          filter: {
+            geo_shape: {
+              service_area_location: {
+                relation: "intersects",
+                shape: {
+                  type: 'point',
+                  coordinates: [lon, lat]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    logger.debug query.to_yaml
+
+    result = Store.__elasticsearch__.search(query)
+    return result.count>0
+  end
+
+  include ElasticsearchSearchable
+
+  def service_area_location
+    coordinates = self.service_area.split(',')
+    coordinates = (0..coordinates.length-1).step(2).map do |i|
+      [coordinates[i], coordinates[i+1]]
+    end
+
+    {
+      type: 'polygon',
+      coordinates: [coordinates]
+    }
+  end
+
+  # elasticsearch settings
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'strict' do
+      indexes :name
+      indexes :address
+      indexes :phone
+      indexes :description
+      indexes :service_area_location, type: 'geo_shape', tree: 'geohash', precision: '1m'
+    end
+  end
+
+  def as_indexed_json(options={})
+    self.as_json(only: [:name, :address, :phone, :description], methods: :service_area_location)
+  end
 end
+
+Store.import force: true
