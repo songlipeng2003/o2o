f05180fc74f4d5f557f69e53d25b2ffeb3c3a75d
Thinking Song <123wuming_123@163.com>
2015-01-21 00:26:10 +0800

    添加预约时间说明

--- app/api/v1/orders.rb
+++ app/api/v1/orders.rb
@@ -120,7 +120,7 @@ module V1
           optional :lat, type: String, desc: "纬度"
         end
         mutually_exclusive :address, :address_id
-        requires :booked_at, type: String, desc: "预约时间"
+        requires :booked_at, type: String, desc: "预约时间，时间格式2014-01-01 01:01:00, 为预约的起始时间"
         optional :product_id, type: Integer, desc: "商品编号，默认为1标准洗车"
         optional :is_underground_park, type: Boolean, desc: "是否在地下停车库"
         optional :carport, type: String, desc: "车位号"
