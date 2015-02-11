8bb80b787fc36d4ea3e511c8321c8b2f53451063
Thinking Song <123wuming_123@163.com>
2015-02-12 00:03:07 +0800

    修改服务时间备注

--- app/api/v1/stores.rb
+++ app/api/v1/stores.rb
@@ -18,7 +18,7 @@ module V1
       params do
         requires :lon, type: Float, desc: "经度"
         requires :lat, type: Float, desc: "纬度"
-        requires :booked_at, type: String, desc: "服务时间"
+        requires :booked_at, type: String, desc: "服务时间,时间格式2015-02-11 00:00:00，为服务开始时间"
       end
       get 'can_serviced' do
         result = Store.can_serviced(params[:lon], params[:lat], params[:booked_at])
