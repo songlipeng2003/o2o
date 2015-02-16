bbd59ce2aa84f376765af8c1fa66efb2fde7e99a
Thinking Song <123wuming_123@163.com>
2015-02-16 12:32:58 +0800

    增加登录历史管理

---
+++ app/admin/login_history.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register LoginHistory do
+  menu parent: '用户'
+
+  actions :index
+
+  index do
+    id_column
+    column :user
+    column :device
+    column :device_type
+    column :device_model
+    column :ip
+    column :created_at
+    actions
+  end
+end
