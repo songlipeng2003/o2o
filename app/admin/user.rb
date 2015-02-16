74617c1695ed3dee178c6f7294bfe44e9ee851aa
Thinking Song <123wuming_123@163.com>
2015-02-16 15:28:47 +0800

    用户列表增加显示用户余额积分

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -9,6 +9,8 @@ ActiveAdmin.register User do
     column :phone
     column :nickname
     column :gender
+    column :balance
+    column :score
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
