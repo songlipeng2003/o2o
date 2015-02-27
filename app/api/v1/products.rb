8d759efc6cf536bdd8d07165a60a372463774b30
Thinking Song <123wuming_123@163.com>
2015-02-27 23:11:32 +0800

    标准洗车不显示在保养列表里

--- app/api/v1/products.rb
+++ app/api/v1/products.rb
@@ -3,7 +3,7 @@ module V1
     resource :products do
       desc "商品列表"
       get do
-        products = Product.all
+        products = Product.where('id>2').all
         present products, with: V1::Entities::Product
       end
     end
