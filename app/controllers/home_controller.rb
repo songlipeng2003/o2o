4a03cb9f4ceaf4345898d7e60030f88ba7704e42
Thinking Song <123wuming_123@163.com>
2015-02-28 01:14:18 +0800

    增加关于我们页面

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    render layout: false
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def download
     render layout: false
   end
+
+  def about
+  end
 end
