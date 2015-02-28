d3bd64229f37ed6a881c2591899534bbcffd5b06
Thinking Song <123wuming_123@163.com>
2015-02-28 13:19:53 +0800

    增加地址管理

---
+++ app/admin/address.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Address do
+
+  actions :index, :show, :edit, :destroy
+
+  permit_params :name, :address, :area_id, :lon, :lat
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :name
+    column :place
+    actions
+  end
+
+  filter :name
+  filter :place
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :place
+      f.input :lon
+      f.input :lat
+    end
+    f.actions
+  end
+end
