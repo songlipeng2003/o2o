990df0b4c8f68646a589fce5296f64ba58dd5d86
Thinking Song <123wuming_123@163.com>
2015-03-07 18:56:10 +0800

    add payment_log manage

---
+++ app/admin/payment_log.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register PaymentLog do
+  menu parent: '财务'
+
+  actions :index, :show
+
+  config.batch_actions = false
+
+  index do
+    id_column
+    column :item
+    column :payment
+    column :state do |order|
+      order.aasm.human_state
+    end
+    column :application
+    column :created_at
+    actions
+  end
+end
