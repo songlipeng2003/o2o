5feb055097f97a4b47b7095312f51a261d03e421
Thinking Song <123wuming_123@163.com>
2015-03-09 21:36:11 +0800

    update alipay config

--- config/initializers/alipay.rb
+++ config/initializers/alipay.rb
@@ -1,3 +1,3 @@
-Alipay.pid = 'YOUR_PID'
-Alipay.key = 'YOUR_KEY'
-Alipay.seller_email = 'YOUR_SELLER_EMAIL'
+Alipay.pid = '2088712939438062'
+Alipay.key = '20altid7ugsn8hm06njq30d85y6bma0a'
+Alipay.seller_email = 'didi@24didi.com'
