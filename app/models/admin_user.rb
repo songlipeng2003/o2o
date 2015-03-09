14745258927ad13091b96e71feebbaa34acf531c
Thinking Song <123wuming_123@163.com>
2015-03-09 12:53:08 +0800

    add admin login log

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,4 +6,15 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def update_tracked_fields!(request)
+    self.login_histories.create({
+      ip: request.env['REMOTE_ADDR'],
+      device: request.env['REMOTE_ADDR'],
+      device_model: request.env['HTTP_USER_AGENT'],
+      device_type: 'web'
+    })
+    super(request)
+  end
 end
