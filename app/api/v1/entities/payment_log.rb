0ca8457cfbfbbc183ac93a9130810bb5722cf15d
Thinking Song <123wuming_123@163.com>
2015-03-10 16:25:07 +0800

    add pay_params to alipay

--- app/api/v1/entities/payment_log.rb
+++ app/api/v1/entities/payment_log.rb
@@ -1,3 +1,6 @@
+require 'openssl'
+require 'base64'
+
 module V1
   module Entities
     class PaymentLog < Grape::Entity
@@ -23,6 +26,34 @@ module V1
 
           token = Alipay::Service::Wap.trade_create_direct_token(options)
           Alipay::Service::Wap.auth_and_execute(request_token: token)
+        end
+      end
+      expose :pay_params do |payment_log|
+        if payment_log.payment.code = 'alipay_app'
+          params = {}
+          params['partner'] = Alipay.pid
+          params['seller_id'] = Alipay.seller_email
+          params['out_trade_no'] = payment_log.id
+          params['subject'] = payment_log.name
+          params['body'] = payment_log.name
+          params['total_fee'] = payment_log.amount
+          params['notify_url'] = 'http://24didi.com/pay/alipay_app_notify'
+          params['service'] = 'mobile.securitypay.pay'
+          params['payment_type'] = 1
+          params['_input_charset'] = 'utf-8'
+          params['it_b_pay'] = '30m'
+          # params['return_url'] = ''
+
+          params = Alipay::Utils.stringify_keys(params)
+
+          to_sign = params.map { |item| "#{item[0]}=\"#{item[1]}\"" }.join('&')
+
+          keypair = OpenSSL::PKey::RSA.new File.read File.join('config', 'rsa_private_key.pem')
+          digest = OpenSSL::Digest::SHA1.new
+          signature = keypair.sign(digest, to_sign)
+          signature = Base64.encode64(signature)
+
+          "#{to_sign}&sign=\"#{signature}\"&sign_type=\"RSA\""
         end
       end
     end
