a92a9d829b785bde01a4bd78505bc6934c9d342a
Thinking Song <123wuming_123@163.com>
2015-03-22 17:50:18 +0800

    add TradingRecord Entity

--- app/api/store_v1/entities/trading_record.rb
+++ app/api/store_v1/entities/trading_record.rb
@@ -1,1 +1,15 @@
+module StoreV1
+  module Entities
+    class TradingRecord < Grape::Entity
+      expose :id, documentation: { type: Integer, desc: '编号' }
+      expose :name, documentation: { type: String, desc: '名称' }
+      expose :trading_type, documentation: { type: Integer, desc: '交易类型' }
+      expose :trading_type_name, documentation: { type: String, desc: '交易类型名称' }
+      expose :amount, documentation: { type: Float, desc: '金额' }
+      expose :start_amount, documentation: { type: Float, desc: '起始金额' }
+      expose :end_amount, documentation: { type: Float, desc: '结束金额' }
+      expose :remark, documentation: { type: String, desc: '备注' }
+      expose :created_at, documentation: { type: Time, desc: '创建时间' }
     end
+  end
+end
