be5cba2e73d6ed02c920e671ccdc208e04d2e786
Thinking Song <123wuming_123@163.com>
2015-03-24 23:59:27 +0800

    update product api

--- app/api/v1/products.rb
+++ app/api/v1/products.rb
@@ -2,8 +2,19 @@ module V1
   class Products < Grape::API
     resource :products do
       desc "商品列表"
+      params do
+        optional :city_id, type: Integer, default: 917, desc: '城市编号，默认为917郑州'
+        optional :product_type, type: Integer, values: Product::PRODUCT_TYPES.keys, default: 1,
+          desc: "商品类型，1为上门商品，2为到店商品，默认为上门"
+      end
       get do
-        products = Product.where('id>2').all
+        products = Product.where('0=0')
+        if params[:city_id]
+          products = products.joins(:store).where(stores: { city_id: params[:city_id] })
+        end
+        if params[:product_type]
+          products = products.where(product_type: params[:product_type])
+        end
         present products, with: V1::Entities::Product
       end
     end
