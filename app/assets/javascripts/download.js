b3af0b20b6b8e803f6601861376823552c8ee275
Thinking Song <123wuming_123@163.com>
2015-03-26 23:19:07 +0800

    add download auto redirect

---
+++ app/assets/javascripts/download.js
@@ -0,0 +1,42 @@
+//= require jquery
+//= require_self
+var isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+};
+
+function isWeiXin(){
+  if(navigator.userAgent.toLowerCase().match(/MicroMessenger/i) == 'micromessenger'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+$(function(){
+  if (isMobile.Android()) {
+    if (isWeiXin()) {
+      location.href = "http://sj.qq.com/myapp/detail.htm?apkName=com.didi361.didi";
+    }else{
+      location.href = $('#android').attr('href');
+    }
+  }else if(isMobile.iOS()) {
+    location.href = $('#ios').attr('href');
+  }
+});
