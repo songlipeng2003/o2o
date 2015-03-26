cb04b4fa85e5678ee2f2fd92f585333ae2069feb
Thinking Song <123wuming_123@163.com>
2015-03-26 23:30:33 +0800

    change validate to validates

--- app/models/app_version.rb
+++ app/models/app_version.rb
@@ -1,6 +1,6 @@
 class AppVersion < ActiveRecord::Base
-  validate :file, presence: true
-  validate :version, presence: true
+  validates :file, presence: true
+  validates :version, presence: true
 
   mount_uploader :file, AppVersionFileUploader
 end
