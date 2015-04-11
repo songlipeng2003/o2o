cc49329591347c3f579ea4869e55fbb4a917c4b3
Thinking Song <123wuming_123@163.com>
2015-04-11 23:23:30 +0800

    add category_id for product  api

--- app/api/v1/products.rb
+++ app/api/v1/products.rb
@@ -4,6 +4,7 @@ module V1
       desc "商品列表"
       params do
         optional :city_id, type: Integer, default: 917, desc: '城市编号，默认为917郑州'
+        optional :category_id, type: Integer, desc: '分类编号'
         optional :product_type, type: Integer, values: Product::PRODUCT_TYPES.keys, default: 1,
           desc: "商品类型，1为上门商品，2为到店商品，默认为上门"
       end
@@ -11,6 +12,9 @@ module V1
         products = Product.where('0=0')
         if params[:city_id]
           products = products.joins(:store).where(stores: { city_id: params[:city_id] })
+        end
+        if params[:category_id]
+          products = products.where(category_id: params[:category_id])
         end
         if params[:product_type]
           products = products.where(product_type: params[:product_type])
