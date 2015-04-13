7e679e5217ead00b89ff7b1d565559690d118962
Thinking Song <123wuming_123@163.com>
2015-04-13 21:57:02 +0800

    remove product_type param from system product api

--- app/api/v1/entities/system_product.rb
+++ app/api/v1/entities/system_product.rb
@@ -2,6 +2,10 @@ module V1
   module Entities
     class SystemProduct < Grape::Entity
       expose :id
+      expose :category_id
+      expose :category_name do |system_product|
+        system_product.category.name
+      end
       expose :name
       expose :description
       expose :image do |product|
--- app/api/v1/system_products.rb
+++ app/api/v1/system_products.rb
@@ -4,17 +4,12 @@ module V1
       desc "系统商品列表"
       params do
         optional :category_id, type: Integer, desc: '分类编号'
-        optional :product_type, type: Integer, values: Product::PRODUCT_TYPES.keys, default: 1,
-          desc: "商品类型，1为上门商品，2为到店商品，默认为上门"
       end
       get do
         system_products = SystemProduct.where('0=0')
         if params[:category_id]
           system_products = system_products.where(category_id: params[:category_id])
         end
-        # if params[:product_type]
-        #   system_products = system_products.where(product_type: params[:product_type])
-        # end
         present system_products, with: V1::Entities::SystemProduct
       end
     end
