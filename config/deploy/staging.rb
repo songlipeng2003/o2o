669b466a68d7bb09faf9171735c519abcef9929c
Thinking Song <123wuming_123@163.com>
2015-04-16 01:09:08 +0800

    add staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,11 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{root@119.254.102.102}
-role :web, %w{root@119.254.102.102}
-role :db,  %w{root@119.254.102.102}
+role :app, %w{root@123.57.58.130}
+role :web, %w{root@123.57.58.130}
+role :db,  %w{root@123.57.58.130}
+
+set :branch, 'v2'
 
 
 # Extended Server Syntax
@@ -15,7 +17,7 @@ role :db,  %w{root@119.254.102.102}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '119.254.102.102', user: 'root', roles: %w{web app}
+# server '119.254.102.102', user: 'root', roles: %w{web app}
 
 
 # Custom SSH Options
