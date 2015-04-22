69acd06943bc08323c01670574f7348ff607f55e
Thinking Song <123wuming_123@163.com>
2015-04-22 23:10:38 +0800

    modify weixin auth

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -1,16 +1,14 @@
 class WeixinController < ApplicationController
   def auth
     code = params[:code]
-    redirect_url = params[:url]
-    if code && redirect_url
+    if code
       url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx0babbb28a049562d&secret=9b49714550a5e2bfc906a691ea463fab&code=#{code}&grant_type=authorization_code"
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
 
       result = JSON.parse response.body
 
-
-      redirect_url << '?open_id='+result['open_id']
+      redirect_url = 'http://m.24didi.com?open_id='+result['open_id']
 
       redirect_to redirect_url
     end
