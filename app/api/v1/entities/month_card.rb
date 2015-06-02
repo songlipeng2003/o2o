b6056b8b6245d921cfe2bf7a152dbb3c5584299e
Thinking Song <123wuming_123@163.com>
2015-06-02 13:09:42 +0800

    add name for month card api

--- app/api/v1/entities/month_card.rb
+++ app/api/v1/entities/month_card.rb
@@ -2,6 +2,7 @@ module V1
   module Entities
     class MonthCard < Grape::Entity
       expose :id, documentation: { type: Integer, desc: '编号' }
+      expose :name, documentation: { type: String, desc: '名称' }
       expose :license_tag, documentation: { type: String, desc: '牌照' }
       expose :started_at, documentation: { type: Time, desc: '开始时间' }
       expose :expired_at, documentation: { type: Time, desc: '过期时间' }
