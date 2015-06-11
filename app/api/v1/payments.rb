369872e5d3b6a7d5f46d01d0dfa554ff1b99ee04
Thinking Song <123wuming_123@163.com>
2015-06-11 22:26:36 +0800

    支付方式，消费卡订单不支持余额支付

--- app/api/v1/payments.rb
+++ app/api/v1/payments.rb
@@ -3,13 +3,13 @@ module V1
     resource :payments do
       desc "支付方式列表"
       params do
-        requires :type, type: String, desc: "支付对象，目前可以传 product,recharge"
+        requires :type, type: String, desc: "支付对象，目前可以传 product,recharge,month_card_order"
       end
       get '/', http_codes: [
         [200, 'Ok', V1::Entities::Payment]
       ]  do
         payemnts = current_application.payments
-        if params[:type]=='recharge'
+        if params[:type]=='recharge' || params[:type]=='month_card_order'
           payemnts = payemnts.where.not(code: 'balance')
         end
         present payemnts, with: V1::Entities::Payment
