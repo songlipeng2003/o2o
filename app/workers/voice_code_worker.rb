31a534446a99d850bf7ac1b348fa21af447d064e
Thinking Song <123wuming_123@163.com>
2015-06-30 00:32:04 +0800

    add voice code

---
+++ app/workers/voice_code_worker.rb
@@ -0,0 +1,20 @@
+require "uri"
+require "net/http"
+
+class VoiceCodeWorker
+  include Sidekiq::Worker
+
+  def perform(phone, code)
+    params = {
+      apikey: Settings.yunpian.password,
+      mobile: phone,
+      code: code
+    }
+
+    reponse = Net::HTTP.post_form(URI.parse('http://voice.yunpian.com/v1/voice/send.json'), params)
+    data = JSON.parse(reponse.body)
+    if data['code']!=0
+      logger.error(data['msg'])
+    end
+  end
+end
