fd3532f5467dd8d73e680e2cc7f7ea1ed5a4f1ce
Thinking Song <123wuming_123@163.com>
2015-08-08 23:20:16 +0800

    月卡修改为消费卡

--- app/api/v1/entities/order_price.rb
+++ app/api/v1/entities/order_price.rb
@@ -5,7 +5,7 @@ module V1
       expose :total_amount, documentation: { type: Float, desc: '总价' }
       expose :order_amount, documentation: { type: Float, desc: '订单价格' }
       expose :service_ticket_id, documentation: { type: Integer, desc: '服务器编号' }
-      expose :month_card_id, documentation: { type: Integer, desc: '月卡编号' }
+      expose :month_card_id, documentation: { type: Integer, desc: '消费卡编号' }
     end
   end
 end
