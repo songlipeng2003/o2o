d19d5df2a36dfb5759c9376c8c2fbe268667acd5
Thinking Song <123wuming_123@163.com>
2015-08-15 19:50:56 +0800

    晚间洗车价格都是5.9

--- app/models/order.rb
+++ app/models/order.rb
@@ -207,7 +207,7 @@ class Order < ActiveRecord::Base
       # 夜间洗车费用
       if booked_at && booked_at.hour >= 20
         if car_model.auto_type == 'SUV'
-          price = 7.9
+          price = 5.9
         else
           price = 5.9
         end
