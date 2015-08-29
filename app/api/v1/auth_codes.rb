80856ad90d6d8abc141a20a6e29a2259003c33d6
Thinking Song <123wuming_123@163.com>
2015-08-29 21:29:23 +0800

    update sms template id

--- app/api/v1/auth_codes.rb
+++ app/api/v1/auth_codes.rb
@@ -24,7 +24,7 @@ module V1
 
         code = AuthCode.generate phone
         tpl_params = { code: code}
-        SMSWorker.perform_async(phone, 671255, tpl_params)
+        SMSWorker.perform_async(phone, 943151, tpl_params)
 
         {
           code: 0,
