c379f79e490fff9e2031d831cba9b7c70509fd2c
Thinking Song <123wuming_123@163.com>
2016-03-17 22:39:46 +0800

    add more charts

--- app/admin/order_report.rb
+++ app/admin/order_report.rb
@@ -2,9 +2,34 @@ ActiveAdmin.register_page "Order Report" do
   menu parent: '统计', priority: 1, label: '订单统计'
 
   content title: '订单统计' do
-    data = Order.group(:application_id).count.map do |k, v|
-      [Application.find(k).name, v]
+    panel '订单来源统计' do
+      pie_chart Order.group(:application_id).count.map do |k, v|
+        [Application.find(k).name, v]
+      end
     end
-    pie_chart data
+
+    panel '每日订单量统计' do
+      line_chart Order.where(state: [:payed, :confirmed, :finished]).where('created_at>?', 1.years.ago).group_by_day(:created_at).count
+    end
+
+    panel '每日订单额统计' do
+      line_chart Order.where(state: [:payed, :confirmed, :finished]).where('created_at>?', 1.years.ago).group_by_day(:created_at).sum(:total_amount)
+    end
+
+    panel '每周订单量统计' do
+      line_chart Order.where(state: [:payed, :confirmed, :finished]).where('created_at>?', 1.years.ago).group_by_week(:created_at).count
+    end
+
+    panel '每周订单额统计' do
+      line_chart Order.where(state: [:payed, :confirmed, :finished]).where('created_at>?', 1.years.ago).group_by_week(:created_at).sum(:total_amount)
+    end
+
+    panel '每月订单量统计' do
+      line_chart Order.where(state: [:payed, :confirmed, :finished]).where('created_at>?', 1.years.ago).group_by_month(:created_at).count
+    end
+
+    panel '每月订单额统计' do
+      line_chart Order.where(state: [:payed, :confirmed, :finished]).where('created_at>?', 1.years.ago).group_by_month(:created_at).sum(:total_amount)
+    end
   end
 end
--- app/admin/recharge_report.rb
+++ app/admin/recharge_report.rb
@@ -2,9 +2,34 @@ ActiveAdmin.register_page "Recharge Report" do
   menu parent: '统计', priority: 1, label: '充值统计'
 
   content title: '充值统计' do
-    data = Recharge.group(:application_id).count.map do |k, v|
-      [Application.find(k).name, v]
+    panel '来源统计' do
+      pie_chart Recharge.group(:application_id).count.map do |k, v|
+        [Application.find(k).name, v]
+      end
     end
-    pie_chart data
+
+    panel '每日充值量统计' do
+      line_chart Recharge.where(state: :payed).where('created_at>?', 1.years.ago).group_by_day(:created_at).count
+    end
+
+    panel '每日充值额统计' do
+      line_chart Recharge.where(state: :payed).where('created_at>?', 1.years.ago).group_by_day(:created_at).sum(:amount)
+    end
+
+    panel '每周充值量统计' do
+      line_chart Recharge.where(state: :payed).where('created_at>?', 1.years.ago).group_by_week(:created_at).count
+    end
+
+    panel '每周充值额统计' do
+      line_chart Recharge.where(state: :payed).where('created_at>?', 1.years.ago).group_by_week(:created_at).sum(:amount)
+    end
+
+    panel '每月充值量统计' do
+      line_chart Recharge.where(state: :payed).where('created_at>?', 1.years.ago).group_by_month(:created_at).count
+    end
+
+    panel '每月充值额统计' do
+      line_chart Recharge.where(state: :payed).where('created_at>?', 1.years.ago).group_by_month(:created_at).sum(:amount)
+    end
   end
 end
