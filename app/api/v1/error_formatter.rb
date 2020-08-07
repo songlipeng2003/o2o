0ad2e61b0b9509b76b1aed223f9303de01504b67
Thinking Song <123wuming_123@163.com>
2020-08-07 22:56:39 +0800

    Update error_formatter.rb

--- app/api/v1/error_formatter.rb
+++ app/api/v1/error_formatter.rb
@@ -1,6 +1,6 @@
 module V1
   module ErrorFormatter
-    def self.call message, backtrace, options, env
+    def self.call message, backtrace, options, env, original_exception
       { :response_type => 'error', :response => message }.to_json
     end
   end
